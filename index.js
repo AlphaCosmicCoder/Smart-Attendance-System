e5ce7102b8cb6892ee006a00d7b9f0e94bb08f5c
099ayuh <hiayushsingh.co.in@gmail.com>
2023-05-01 15:55:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,217 @@
+const express = require('express');
+const fs = require('fs')
+const app = express();
+const ejs = require('ejs');
+const cors = require('cors');
+
+
+function chooseCardId() {
+    const cards = JSON.parse(fs.readFileSync('card.json'));
+
+    // filter out cards where status is "assigned"
+    const unassignedCards = cards.filter(card => card.status !== 'assigned');
+
+    if (unassignedCards.length === 0) {
+        return -1;
+    }
+    // choose a random card from the unassigned cards
+    const randomIndex = Math.floor(Math.random() * unassignedCards.length);
+    const randomCard = unassignedCards[randomIndex];
+
+    // return the chosen card ID
+    return randomCard.id;
+}
+
+
+function assignCard(cardId) {
+    const cards = JSON.parse(fs.readFileSync('card.json'));
+
+    // find the index of the card with the given ID
+    const cardIndex = cards.findIndex(card => card.id === cardId);
+
+    // if the card was found, update its status to "assigned"
+    if (cardIndex !== -1) {
+        cards[cardIndex].status = 'assigned';
+        fs.writeFileSync('card.json', JSON.stringify(cards));
+        console.log(`Card ${cardId} assigned.`);
+    } else {
+        console.log(`Card ${cardId} not found.`);
+    }
+}
+
+function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+function getCurrentDate() {
+    const now = new Date();
+    const year = now.getFullYear().toString().padStart(4, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    const name = 'John';
+    res.render('index');
+});
+
+app.get('/dailyreport', (req, res) => {
+    res.render('report', {
+        title: "form"
+    })
+})
+
+app.post('/dailyreport', (req, res) => {
+    let currentDate = req.body.date
+    const attendance = JSON.parse(fs.readFileSync('attendance.json'));
+    const AttendanceRecord = attendance.filter(record => record.Date === currentDate);
+    if (AttendanceRecord.length > 0) {
+        // res.json(AttendanceRecord);
+        res.render('report', {
+            title: "Attendence Data",
+            date: currentDate,
+            attendence: AttendanceRecord[0].Record
+        })
+    } else {
+        res.render('report', {
+            title: "Attendence Data",
+            date: currentDate,
+            attendence: '404 not found'
+        })
+    }
+})
+
+app.get('/register', (req, res) => {
+    res.render('studentRegister')
+})
+
+app.post('/register', (req, res) => {
+    let student = req.body;
+    let studentRecord = {
+        "roll": student.roll,
+        "fname": student.fname,
+        "lname": student.lname,
+        "department": student.department,
+        "scode": student.scode,
+        "card": chooseCardId()
+    }
+
+    if (studentRecord.card === -1) {
+        res.status(404).send("nahi jagah hai")
+    } else {
+        assignCard(studentRecord.card)
+        const cards = JSON.parse(fs.readFileSync('students.json'));
+        cards.push(studentRecord);
+        fs.writeFileSync('students.json', JSON.stringify(cards));
+        res.send(`Card ${studentRecord.fname} ${studentRecord.lname} added.`)
+    }
+})
+
+
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    console.log(req.body.rfid)
+    let id = req.body.rfid;
+    let card = {
+        "id": id,
+        "status": "notassigned"
+    }
+    const cards = JSON.parse(fs.readFileSync('card.json'));
+    const recordedCards = cards.filter(card => card.id === id);
+    if (recordedCards.length === 0) {
+        cards.push(card);
+        fs.writeFileSync('card.json', JSON.stringify(cards));
+        res.send(`Card ${id} recorded.`)
+    } else {
+        res.send("Card Already Added")
+    }
+
+    /*  fs.writeFile('database.json', JSON.stringify(req.body), (err) => {
+         if (err) {
+             console.error(err);
+             res.status(500).send('Error writing JSON data to file');
+         } else {
+             console.log('JSON data written to file');
+             res.status(200).send('JSON data written to file');
+         }
+     }); */
+})
+
+app.post('/startattendance', (req, res) => {
+    /* const attendancerecord = {
+        "Date": getCurrentDate(),
+        "Record": []
+    }
+    const records = JSON.parse(fs.readFileSync('attendance.json'));
+    records.push(attendancerecord);
+    fs.writeFileSync('attendance.json', JSON.stringify(records));
+    res.send('attendance started') */
+    const currentDate = getCurrentDate();
+    const attendance = JSON.parse(fs.readFileSync('attendance.json'));
+    const currentAttendanceRecord = attendance.find(record => record.Date === currentDate);
+    if (currentAttendanceRecord) {
+        res.status(409).send('Attendance for today has already been started');
+    } else {
+        const newAttendanceRecord = {
+            "Date": currentDate,
+            "Record": []
+        }
+        attendance.push(newAttendanceRecord);
+        fs.writeFileSync('attendance.json', JSON.stringify(attendance));
+        res.send('Attendance for today has been started');
+    }
+})
+
+app.post('/addattendance', (req, res) => {
+    const students = JSON.parse(fs.readFileSync('students.json'));
+    const student = students.filter(student => student.card === req.body.rfid)
+    if (Object.keys(student).length === 0) {
+        res.status(404).send("No Student Record Found");
+    } else {
+        console.log(student)
+        const attendancerecord = {
+            "roll": student[0].roll,
+            "fname": student[0].fname,
+            "lname": student[0].lname,
+            "department": student[0].department,
+            "scode": student[0].scode,
+            "time": getCurrentTime()
+        }
+
+        const attendance = JSON.parse(fs.readFileSync('attendance.json'));
+        const recordtoday = attendance.filter(attendance => attendance.Date === getCurrentDate())
+        console.log(recordtoday)
+        // recordtoday[0].Record.push(attendancerecord);
+        // fs.writeFileSync('attendance.json', JSON.stringify(recordtoday));
+        // res.send("attendence recorder");
+        const studentRecordExists = recordtoday[0].Record.some(record => record.roll === student[0].roll);
+        if (studentRecordExists) {
+            res.status(409).send(`${student[0].fname} ${student[0].lname} has already been marked present for today`);
+        } else {
+            recordtoday[0].Record.push(attendancerecord);
+            fs.writeFileSync('attendance.json', JSON.stringify(attendance));
+            res.status(200).send(`${student[0].fname} ${student[0].lname} marked present`);
+        }
+    }
+})
+
+app.get('/addattendance', (req, res) => {
+    const attendance = JSON.parse(fs.readFileSync('attendance.json'));
+    const recordtoday = attendance.filter(attendance => attendance.Date === getCurrentDate());
+    res.send(recordtoday);
+})
+
+app.listen(3000, () => {
+    console.log(`http://192.168.59.134:3000`);
+})
---
+++ rfid.ino
@@ -0,0 +1,134 @@
+#ifdef ESP32
+#include <WiFi.h>
+#include <HTTPClient.h>
+#else
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#endif
+#include <SPI.h>
+#include <MFRC522.h>
+
+const char* ssid = "Ayush";
+const char* password = "ayush099";
+WiFiClient wifiClient;
+String serverName = "http://192.168.59.134:3000/addattendance";
+
+#define SS_PIN D4
+#define RST_PIN D0
+MFRC522 rfid(SS_PIN, RST_PIN);  // Instance of the class
+MFRC522::MIFARE_Key key;
+// Init array that will store new NUID
+byte nuidPICC[4];
+void setup() {
+  pinMode(D1, OUTPUT);
+  Serial.begin(9600);
+  SPI.begin();  // Init SPI bus
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  Serial.println(WiFi.status());
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  rfid.PCD_Init();  // Init MFRC522
+  Serial.println();
+  Serial.print(F("Reader :"));
+  rfid.PCD_DumpVersionToSerial();
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  Serial.println();
+  Serial.println(F("This code scan the MIFARE Classic NUID."));
+  Serial.print(F("Using the following key:"));
+  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+}
+void loop() {
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if (!rfid.PICC_IsNewCardPresent())
+    return;
+  // Verify if the NUID has been readed
+  if (!rfid.PICC_ReadCardSerial())
+    return;
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] || rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3]) {
+    Serial.println(F("A new card has been detected."));
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+    Serial.println(F("The NUID tag is:"));
+    Serial.print(F("In hex: "));
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+
+    String uid = "";
+    for (byte i = 0; i < rfid.uid.size; i++) {
+      uid += String(rfid.uid.uidByte[i], HEX);
+    }
+
+    Serial.println("RFID UID: " + uid);
+    sendToNodeJS(uid);
+
+
+    // Serial.print(F("In dec: "));
+    // printDec(rfid.uid.uidByte, rfid.uid.size);
+    // Serial.println();
+  } else Serial.println(F("Card read previously."));
+  // Halt PICC
+  rfid.PICC_HaltA();
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
+/**
+ 		Helper routine to dump a byte array as hex values to Serial.
+*/
+void printHex(byte* buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+/**
+ 		Helper routine to dump a byte array as dec values to Serial.
+*/
+void printDec(byte* buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
+}
+
+void sendToNodeJS(String uid) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(wifiClient, serverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    String httpRequestData = "&rfid="+ uid +"&status=approved";
+    int httpCode = http.POST(httpRequestData);
+    if(httpCode == 404) {
+      digitalWrite(D1, HIGH);
+      delay(1000);
+      digitalWrite(D1, LOW);
+      delay(1000);
+    }
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpCode);
+    Serial.println(http.getString());
+
+    // Free resources
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+}
